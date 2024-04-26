b6c57d99d5472f071b646f52f46f848560f856d1
wenliangyeoh <wlyeoh@cc.saga-u.ac.jp>
2024-04-26 19:30:15 +0900

    remove distutils

--- motion_analysis_2d/custom_components/gui_save_base.py
+++ motion_analysis_2d/custom_components/gui_save_base.py
@@ -1,5 +1,4 @@
 import inspect
-from distutils.util import strtobool
 
 from motion_analysis_2d.custom_components.path_edit import PathEdit
 from motion_analysis_2d.defs import QtWidgets
@@ -58,3 +57,19 @@ class BaseGuiSave:
                     obj.setChecked(strtobool(value))
                 elif isinstance(obj, QtWidgets.QComboBox):
                     obj.setCurrentText(value)
+
+
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return 1
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
